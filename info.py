5b9fb00825e90a8e6d4b0d764ca0b657e5434cae
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-05-10 15:00:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "") #if heroku then paste the app link here ex: https://heroku......./
+URL = environ.get("URL", "https://rishikeshfilerbot-8078cfe993c7.herokuapp.com/") #if heroku then paste the app link here ex: https://heroku......./
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
